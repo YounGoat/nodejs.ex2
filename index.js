2d8d99689efcf6ee0b46f4ecbc0b9eb4eb040e4e
YounGoat <youngoat@163.com>
2016-12-12 17:30:53 +0800

    Auto committed by yuan-npm-release

--- index.js
+++ index.js
@@ -6,35 +6,23 @@ var MODULE_REQUIRE
 	, yuan = require('yuan')
 
 	/* in-package */
+	, ExpressUtil = require('./util/ExpressUtil')
 	;
 
-// Judge if "foo" is created according to "proto".
-// 判断 foo 是否仿照 proto 创建。
-// express 中使用了克隆方式创建相关对象，故无法通过对象的原型判断其类别。
-// @TODO 判断逻辑不严谨，可能发生误判。
-function _is_proto_of(proto, foo) {
-	var matched = true;
-	for (var name in proto) {
-		// Sometimes, one is null, and the other one is undefined.
-		if (!foo.hasOwnProperty(name) && !foo[name] && proto[name]) {
-			matched = false;
-			break;
-		}
-	}
-	return matched;
-}
 
 // Core function, return wrapped and expanded instance of Express objects.
 function ex2(what) {
-	var protoname;
-	for (var name in Ex2) {
-		var proto = express[name];
-		if (_is_proto_of(proto, what)) {
-			protoname = name;
-			break;
-		}
-	}
 	if (!what.__yuan_express_extend) {
+		var protoname;
+		for (var name in Ex2) {
+			if (ExpressUtil.prototypeof(what, name)) {
+				protoname = name;
+				break;
+			}
+		}
+		if (!protoname) {
+			throw 'Unsupported instance to be expanded by ex2';
+		}
 		what.__yuan_express_extend = new Ex2[protoname](what);
 	}
 	return what.__yuan_express_extend;
---
+++ util/ExpressUtil.js
@@ -0,0 +1,86 @@
+var MODULE_REQUIRE
+	/* built-in */
+
+	/* NPM */
+	, express = require('express')
+	/* in-package */
+	;
+
+var METHODS = [
+	  'checkout'
+	, 'copy'
+	, 'delete'
+	, 'get'
+	, 'head'
+	, 'lock'
+	, 'merge'
+	, 'mkactivity'
+	, 'mkcol'
+	, 'move'
+	, 'm-search'
+	, 'notify'
+	, 'options'
+	, 'patch'
+	, 'post'
+	, 'purge'
+	, 'put'
+	, 'report'
+	, 'search'
+	, 'subscribe'
+	, 'trace'
+	, 'unlock'
+	, 'unsubscribe'
+];
+
+var CLASS_ALIAS = {
+	'router': 'Router'
+};
+
+var ExpressUtil = {};
+
+// Judge if "foo" is created according to "proto".
+// 判断 foo 是否仿照 proto 创建。
+// express 中使用了克隆方式创建相关对象，故无法通过对象的原型判断其类别。
+// @TODO 判断逻辑不严谨，可能发生误判。
+ExpressUtil.prototypeof = function(instance, protoname) {
+	if (CLASS_ALIAS[protoname]) {
+		protoname = CLASS_ALIAS[protoname];
+	}
+
+	var matched = true;
+	var proto = express[protoname];
+	for (var name in proto) {
+		// Sometimes, one is null, and the other one is undefined.
+		if (!instance.hasOwnProperty(name) && !instance[name] && proto[name]) {
+			matched = false;
+			break;
+		}
+	}
+	return matched;
+};
+
+// Create an express Router instance.
+ExpressUtil.parseRouter = function(router) {
+	var expressRouter;
+
+	if (ExpressUtil.prototypeof(router, 'router')) {
+		expressRouter = router;
+	}
+	else {
+		expressRouter = express.Router();
+
+		METHODS.forEach(function(method) {
+			if (router[method]) {
+				expressRouter[method]('/', router[method]);
+			}
+		});
+
+		// If no method matching the request, try the default.
+		if (typeof router == 'function') {
+			expressRouter.all('/', router);
+		}
+	}
+	return expressRouter;
+};
+
+module.exports = ExpressUtil;
